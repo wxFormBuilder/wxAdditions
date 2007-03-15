55c8c100e2169eb449ea4f564ce02c4893b3c8cc
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2007-03-15 20:35:23 +0000

    Premake is now included in the sdk so removed external to it. Updated the batch build files to make up for the premake move into the sdk.

--- plugins/additions/trunk/wxfbPlugin/create_build_files.sh
+++ plugins/additions/trunk/wxfbPlugin/create_build_files.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-premake/premake-linux --target cb-gcc --unicode --with-wx-shared
+sdk/premake/premake-linux --target cb-gcc --unicode --with-wx-shared
 echo done...
 echo 
 #
-premake/premake-linux --target gnu --unicode --with-wx-shared
+sdk/premake/premake-linux --target gnu --unicode --with-wx-shared
 echo done...
 echo 
 #
