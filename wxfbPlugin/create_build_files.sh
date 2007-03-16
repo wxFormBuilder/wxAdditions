4633bc9ddf891921abc5e7c1dc57adc051332a81
rjmyst3 <ryanjmulder@users.noreply.github.com>
2007-03-16 00:14:29 +0000

    updated to check wx-config, added executable property

--- wxfbPlugin/create_build_files.sh
+++ wxfbPlugin/create_build_files.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 #
-sdk/premake/premake-linux --target cb-gcc --unicode --with-wx-shared
+
+# Autodetect wxWidgets settings
+if wx-config --unicode >/dev/null 2>/dev/null; then
+	unicode="--unicode"
+fi
+if ! wx-config --debug >/dev/null 2>/dev/null; then
+	debug="--disable-wx-debug"
+fi
+
+sdk/premake/premake-linux --target cb-gcc $unicode $debug --with-wx-shared
 echo done...
 echo 
 #
-sdk/premake/premake-linux --target gnu --unicode --with-wx-shared
+sdk/premake/premake-linux --target gnu $unicode $debug --with-wx-shared
 echo done...
 echo 
 #
-echo Done generating all project files for wxAdditions Plugin.
+echo Done generating all project files for wxFormBuilder.
 #
