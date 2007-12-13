40396d6028b6389ee582767d3ecd0427acf597f4
rjmyst3 <rjmyst3@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2007-12-13 02:59:05 +0000

    minor version updates and script tweaks

--- plugins/additions/trunk/wxfbPlugin/install/linux/wxfb-wxadditions_export.sh
+++ plugins/additions/trunk/wxfbPlugin/install/linux/wxfb-wxadditions_export.sh
@@ -27,7 +27,7 @@ CheckAndMakeDir $outputDir/lib/wxformbuilder
   
 cp -R --interactive ../../wxAdditions $outputDir/share/wxformbuilder/plugins/
 mv $outputDir/share/wxformbuilder/plugins/wxAdditions/*.so $outputDir/lib/wxformbuilder/
-cp -R --interactive ../../../lib/gcc_dll/*.so $outputDir/lib/wxformbuilder/
+cp -R --interactive ../../../lib/*.so $outputDir/lib/wxformbuilder/
 rm -f $outputDir/lib/wxformbuilder/*flatnotebook*
 rm -f $outputDir/lib/wxformbuilder/*propgrid*
 rm -f $outputDir/lib/wxformbuilder/*cintilla*
