72520e287d038297907ef027f617cf7ebd4c107d
rjmyst3 <rjmyst3@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2008-02-10 18:53:54 +0000

    added missing license statement

--- plugins/additions/trunk/src/wxScintilla/PlatWX.h
+++ plugins/additions/trunk/src/wxScintilla/PlatWX.h
@@ -1,5 +1,11 @@
-#ifndef PLATWX_H // [CHANGED]
-#define PLATWX_H // [CHANGED]
+// Scintilla source code edit control
+// PlatWX.cxx - implementation of platform facilities on wxWidgets
+// Copyright 1998-1999 by Neil Hodgson <neilh@scintilla.org>
+//                        Robin Dunn <robin@aldunn.com>
+// The License.txt file describes the conditions under which this software may be distributed.
+
+#ifndef PLATWX_H
+#define PLATWX_H
 
 #ifdef SCI_NAMESPACE
 namespace Scintilla
@@ -11,4 +17,4 @@ wxColour wxColourFromCA(const ColourAllocated& ca);
 #ifdef SCI_NAMESPACE
 }
 #endif
-#endif // [CHANGED]
+#endif
