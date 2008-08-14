e7a8279fa2e173192e82ba2f7bca42fafc9c9907
rjmyst3 <ryanjmulder@users.noreply.github.com>
2008-08-14 03:37:05 +0000

    debian directory

---
+++ install/linux/debian/libwxadditions28-default.ex
@@ -0,0 +1,10 @@
+# Defaults for libwxadditions28 initscript
+# sourced by /etc/init.d/libwxadditions28
+# installed at /etc/default/libwxadditions28 by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
